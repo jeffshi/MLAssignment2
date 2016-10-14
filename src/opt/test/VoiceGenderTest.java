be2b62b87d2b3dfcbea119ab6bacb5557d15383a
TracerTong <TracerTong@DESKTOP-6TC0B4A>
2016-10-13 18:06:12 -0700

    renamed some files

--- src/opt/test/VoiceGenderTest.java
+++ src/opt/test/VoiceGenderTest.java
@@ -16,13 +16,14 @@ import java.text.*;
  * find optimal weights to a neural network that is classifying abalone as having either fewer 
  * or more than 15 rings. 
  *
- * @author Hannah Lau
+ * @author Hannah Lau, modified by Jeff Shi for CS7641 Assignment 2
  * @version 1.0
  */
-public class VoiceGenderTest {
-    private static Instance[] instances = initializeInstances();
+public class VoiceGenderTest2 {
+    private static Instance[] instances = initializeInstances("src/opt/test/voice_80split_training_noheaders_binarized.txt");
+    private static Instance[] testing = initializeInstances2("src/opt/test/voice_80split_testing_noheaders_binarized.txt");
 
-    private static int inputLayer = 20, hiddenLayer = 12, outputLayer = 1, trainingIterations = 100;
+    private static int inputLayer = 20, hiddenLayer = 12, outputLayer = 1, trainingIterations = 5000;
     private static BackPropagationNetworkFactory factory = new BackPropagationNetworkFactory();
     
     private static ErrorMeasure measure = new SumOfSquaresError();
@@ -49,6 +50,8 @@ public class VoiceGenderTest {
         oa[1] = new SimulatedAnnealing(1E11, .95, nnop[1]);
         oa[2] = new StandardGeneticAlgorithm(200, 100, 10, nnop[2]);
 
+                
+        String datafile = new String();
         for(int i = 0; i < oa.length; i++) {
             double start = System.nanoTime(), end, trainingTime, testingTime, correct = 0, incorrect = 0;
             train(oa[i], networks[i], oaNames[i]); //trainer.train();
@@ -56,18 +59,20 @@ public class VoiceGenderTest {
             trainingTime = end - start;
             trainingTime /= Math.pow(10,9);
 
+
             Instance optimalInstance = oa[i].getOptimal();
             networks[i].setWeights(optimalInstance.getData());
 
+
             double predicted, actual;
             start = System.nanoTime();
-            for(int j = 0; j < instances.length; j++) {
-                networks[i].setInputValues(instances[j].getData());
+            for(int j = 0; j < testing.length; j++) {
+                networks[i].setInputValues(testing[j].getData());
                 networks[i].run();
 
-                predicted = Double.parseDouble(instances[j].getLabel().toString());
+                predicted = Double.parseDouble(testing[j].getLabel().toString());
                 actual = Double.parseDouble(networks[i].getOutputValues().toString());
-
+                //System.out.println(predicted + " " + actual);
                 double trash = Math.abs(predicted - actual) < 0.5 ? correct++ : incorrect++;
 
             }
@@ -75,52 +80,81 @@ public class VoiceGenderTest {
             testingTime = end - start;
             testingTime /= Math.pow(10,9);
 
-            results +=  "\nResults for " + oaNames[i] + ": \nCorrectly classified " + correct + " instances." +
+            datafile += "\nResults for " + oaNames[i] + ": \nCorrectly classified " + correct + " instances." +
                         "\nIncorrectly classified " + incorrect + " instances.\nPercent correctly classified: "
                         + df.format(correct/(correct+incorrect)*100) + "%\nTraining time: " + df.format(trainingTime)
                         + " seconds\nTesting time: " + df.format(testingTime) + " seconds\n";
-        }
 
+            results +=  oaNames[i]+" ";
+        }
+        try {
+            FileWriter outfile = new FileWriter("results.txt");
+            outfile.write(datafile);
+            outfile.close();
+        } catch (Exception e) {
+
+        }
         System.out.println(results);
     }
 
     private static void train(OptimizationAlgorithm oa, BackPropagationNetwork network, String oaName) {
+        FileWriter outputFile;
+        try {
+            outputFile = new FileWriter(oaName+".txt");
+        } catch (Exception e) {
+            return;
+        }
         System.out.println("\nError results for " + oaName + "\n---------------------------");
 
+        double length = 1773;
+        double incorrect;
         for(int i = 0; i < trainingIterations; i++) {
+            incorrect = 0;
             oa.train();
 
             double error = 0;
-            for(int j = 0; j < instances.length; j++) {
-                network.setInputValues(instances[j].getData());
+            for(int j = 0; j < testing.length; j++) {
+                network.setInputValues(testing[j].getData());
                 network.run();
 
-                Instance output = instances[j].getLabel(), example = new Instance(network.getOutputValues());
+                Instance output = testing[j].getLabel(), example = new Instance(network.getOutputValues());
                 example.setLabel(new Instance(Double.parseDouble(network.getOutputValues().toString())));
                 error += measure.value(output, example);
+                incorrect += Math.abs(Double.parseDouble(output.toString()) - Double.parseDouble(example.getLabel().toString())) < 0.5 ? 0 : 1;
             }
+            try {
+                outputFile.write(df.format(incorrect/length*100));
+                outputFile.write("\n");
+            } catch (Exception e) {
 
+            }
             System.out.println(df.format(error));
+        }
+        try {
+            outputFile.close();
+        } catch (Exception e) {
+
         }
     }
 
-    private static Instance[] initializeInstances() {
+    private static Instance[] initializeInstances(String filename) {
 
         double[][][] attributes = new double[1773][][];
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader(new File("src/opt/test/voice_80split_training_noheaders_binarized.txt")));
+            BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
 
             for(int i = 0; i < attributes.length; i++) {
                 Scanner scan = new Scanner(br.readLine());
                 scan.useDelimiter(",");
 
                 attributes[i] = new double[2][];
-                attributes[i][0] = new double[20]; // 20 attributes
+                attributes[i][0] = new double[20]; // 100 attributes
                 attributes[i][1] = new double[1];
 
-                for(int j = 0; j < 20; j++)
+                for(int j = 0; j < 20; j++){
                     attributes[i][0][j] = Double.parseDouble(scan.next());
+                }
 
                 attributes[i][1][0] = Double.parseDouble(scan.next());
             }
@@ -129,14 +163,50 @@ public class VoiceGenderTest {
             e.printStackTrace();
         }
 
-        Instance[] instances = new Instance[attributes.length];
+        Instance[] data = new Instance[attributes.length];
 
-        for(int i = 0; i < instances.length; i++) {
-            instances[i] = new Instance(attributes[i][0]);
-            // classifications range from 0 to 30; split into 0 - 14 and 15 - 30
-            instances[i].setLabel(new Instance(attributes[i][1][0] < 1 ? 0 : 1));
+        for(int i = 0; i < data.length; i++) {
+            data[i] = new Instance(attributes[i][0]);
+            data[i].setLabel(new Instance(attributes[i][1][0] < .5 ? 0 : 1));
         }
 
-        return instances;
+        return data;
+    }
+    
+    private static Instance[] initializeInstances2(String filename) {
+
+        double[][][] attributes = new double[760][][];
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
+
+            for(int i = 0; i < attributes.length; i++) {
+                Scanner scan = new Scanner(br.readLine());
+                scan.useDelimiter(",");
+
+                attributes[i] = new double[2][];
+                attributes[i][0] = new double[20]; // 100 attributes
+                attributes[i][1] = new double[1];
+
+                for(int j = 0; j < 20; j++){
+                    attributes[i][0][j] = Double.parseDouble(scan.next());
+                }
+
+                attributes[i][1][0] = Double.parseDouble(scan.next());
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        Instance[] data = new Instance[attributes.length];
+
+        for(int i = 0; i < data.length; i++) {
+            data[i] = new Instance(attributes[i][0]);
+            data[i].setLabel(new Instance(attributes[i][1][0] < .5 ? 0 : 1));
+        }
+
+        return data;
     }
 }
+
